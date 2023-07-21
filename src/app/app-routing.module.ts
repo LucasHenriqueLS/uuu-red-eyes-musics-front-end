2a7bd5f06bb34822cee2133c118314c58988c1fc
LucasHenriqueLS <lucashlsl100@gmail.com>
2023-07-21 15:25:13 -0300

    CSS's implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { MusicCreateComponent } from './components/pages/music-create/music-create.component';
+import { SendLyricComponent } from './components/pages/contributions/music-send/send-lyric/send-lyric.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contribuicoes/nome-do-artista/nome-da-musica/enviar/letra', component: SendLyricComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MusicHomeComponent } from './components/pages/music-home/music-home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { MusicCreateComponent } from './components/pages/music-create/music-create.component';
+import { SendLyricComponent } from './components/pages/contributions/music-send/send-lyric/send-lyric.component';
+import { ContributionsComponent } from './components/pages/contributions/contributions.component';
+import { LyricFormComponent } from './components/lyric-form/lyric-form.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MusicHomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    MusicCreateComponent,
+    SendLyricComponent,
+    ContributionsComponent,
+    LyricFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
